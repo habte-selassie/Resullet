5c48035fe30de8f6a72f1d37a6a1de1371981dd2
habte-selassie <habtuchelsea1000@gmail.com>
2024-08-15 18:15:38 +0300

    resolved the index.js file file overwrite issue

--- src/index.js
+++ src/index.js
@@ -1,40 +1,7 @@
-
-import './index.css'
-import { useState, useEffect } from 'react'
-import { createClient } from '@supabase/supabase-js'
-import { Auth } from '@supabase/auth-ui-react'
-import { ThemeSupa } from '@supabase/auth-ui-shared'
-
-
-const supabase = createClient('https://<project>.supabase.co', '<your-anon-key>')
-
-export default function App() {
-  const [session, setSession] = useState(null)
-
-  useEffect(() => {
-    supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-    })
-
-    const {
-      data: { subscription },
-    } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-    })
-
-    return () => subscription.unsubscribe()
-  }, [])
-
-  if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-  }
-  else {
-    return (<div>Logged in!</div>)
-  }
-}
-
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
 
 ReactDOM.render(
