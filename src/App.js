d3a4957980a0b8729f35d4e9c037cfaa288a9d12
JW-dev0505 <shcherbakarina6@gmail.com>
2024-08-16 10:54:43 +0900

    create resume page init

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import AppSupa from './AppSupa'
+// import AppSupa from './AppSupa'
+import { ResumePage } from './pages';
 
 class App extends Component {
   render() {
     return (
-     
+      <ResumePage />
       
 
-        <AppSupa />
+        // <AppSupa />
 
         
     );
   }
-}
+};
 
 export default App;
